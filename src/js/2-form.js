42d894abf4b8dab135e2e39bb96175d140cbcbc9
Kyryl <MaDeR04eK@users.noreply.github.com>
2024-01-02 19:30:14 +0100

    update 2-form file

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,29 +1,34 @@
 const form = document.querySelector('.feedback-form');
 
-const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-  form.email.value = savedState.email || '';
-  form.message.value = savedState.message || '';
+const savedState =
+  JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+form.email.value = (savedState.email || '').trim();
+form.message.value = (savedState.message || '').trim();
 
-  form.addEventListener('input', function (event) {
-      if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-        
-      const currentState = {
-        email: form.email.value,
-        message: form.message.value,
-      };
-      localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
-    }
-  });
-
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const formData = {
-      email: form.email.value,
-      message: form.message.value,
+form.addEventListener('input', function (event) {
+  if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+    const currentState = {
+      email: form.email.value.trim(),
+      message: form.message.value.trim(),
     };
-    console.log('Form Data:', formData);
+    localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+  }
+});
 
-    form.reset();
-    localStorage.removeItem('feedback-form-state');
-  });
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (form.email.value.trim() === '' || form.message.value.trim() === '') {
+    alert('Будь ласка, заповніть обидва поля цієї форми');
+    return;
+  }
+
+  const formData = {
+    email: form.email.value.trim(),
+    message: form.message.value.trim(),
+  };
+  console.log('Form Data:', formData);
+
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+});
